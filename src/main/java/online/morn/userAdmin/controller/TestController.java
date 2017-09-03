32609e0e1d75ba031eace19e9143e64832e69078
Horner <i@morn.online>
2017-09-03 21:57:01 +0800

    Initial commit

---
+++ src/main/java/online/morn/userAdmin/controller/TestController.java
@@ -0,0 +1,13 @@
+package online.morn.userAdmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "welcome";
+    }
+}
